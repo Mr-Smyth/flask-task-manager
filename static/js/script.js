8c53940598178fb577d70d71ec8426050198f12c
Eamonn Smyth <e.smyth75@gmail.com>
2020-09-02 11:17:03 +0000

    Add calendar date picker to add_task page

--- static/js/script.js
+++ static/js/script.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function () {
     var instances = M.Collapsible.init(elems);
     var elems = document.querySelectorAll('.tooltipped');
     var instances = M.Tooltip.init(elems);
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, {format: "dd mmmm, yyyy", yearRange: 3, showClearBtn: true, i18n: {done: "Select"}});
 });
 
 
