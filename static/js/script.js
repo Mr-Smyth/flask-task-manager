d0c9b7bedb6c81be480478beded2c36d76c5cdfd
Eamonn Smyth <e.smyth75@gmail.com>
2020-09-22 13:57:48 +0000

    Tim_CI add conditional checks to check dropdown validation js script

--- static/js/script.js
+++ static/js/script.js
@@ -36,7 +36,8 @@ document.addEventListener('DOMContentLoaded', function () {
          * MATERIALIZE HIDES SELECT ELEMENTS, BUT WE NEED THEM ON THE DOM.
          * AND BECAUSE IT IS ON THE DOM, IT WILL NOT ALLOW SUBMIT, AS IT IS REQUIRED
         */
-        if (selectValidate.hasAttribute("required")) {
+
+        if (selectValidate != null && selectValidate.hasAttribute("required")) {
             selectValidate.style.cssText = "display: block; height: 0; padding: 0; width: 0; position: absolute;";
         }
 
@@ -45,40 +46,42 @@ document.addEventListener('DOMContentLoaded', function () {
          * IF ONE OF THE LIST ITEMS IS SELECTED, BUT DOESNT HAVE THE DISABLED 
          * ATTRIBUTE, THEN APPLY THE CLASS classValid TO MAKE IT GREEN
         */
-        selectWrapperInput.addEventListener("focusin", (e) => {
-            e.target.parentNode.addEventListener("change", () => {
+        if (selectWrapperInput != null) {
+            selectWrapperInput.addEventListener("focusin", (e) => {
+                e.target.parentNode.addEventListener("change", () => {
+                    ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
+                    for (let i = 0; i < ulSelectOptions.length; i++) {
+                        if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled") {
+                            e.target.style.cssText = classValid;
+                        }
+                    }
+                });
+            });
+
+            /** aDD EVENT LISTENER TO WRAPPER INPUT ELEMENTS - FOR A CLICK
+             * WE TRAVERSE THE DOM UP AND DOWN USING PARENT AND CHILD EVENT LISTENERS
+             * IF sELECTED ITEM HAS CLASS OF SELECTED AND IS NOT DISABLED then 
+             * CLASS IS VALID AND SO THE LINE REMAINS GREEN
+             * ELSE IF THE FOCUS LEAVES THE LIST, tHE CLASS BECOMES INVALID, AND
+             * SO THE LINE IS RED.
+             * I THINK THIS IS FOR CASES WHEN REENTERING THE LIST?
+             */
+            selectWrapperInput.addEventListener("click", (e) => {
                 ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
                 for (let i = 0; i < ulSelectOptions.length; i++) {
-                    if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled") {
+                    if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled" && ulSelectOptions[i].style.backgroundColor == "rgba(0, 0, 0, 0.03)") {
                         e.target.style.cssText = classValid;
+                    } else {
+                        e.target.addEventListener("focusout", () => {
+                            if (e.target.parentNode.childNodes[3].hasAttribute("required")) {
+                                if (e.target.style.borderBottom != "1px solid rgb(76, 175, 80)") {
+                                    e.target.style.cssText = classInvalid;
+                                }
+                            }
+                        });
                     }
                 }
             });
-        });
-
-        /** aDD EVENT LISTENER TO WRAPPER INPUT ELEMENTS - FOR A CLICK
-         * WE TRAVERSE THE DOM UP AND DOWN USING PARENT AND CHILD EVENT LISTENERS
-         * IF sELECTED ITEM HAS CLASS OF SELECTED AND IS NOT DISABLED then 
-         * CLASS IS VALID AND SO THE LINE REMAINS GREEN
-         * ELSE IF THE FOCUS LEAVES THE LIST, tHE CLASS BECOMES INVALID, AND
-         * SO THE LINE IS RED.
-         * I THINK THIS IS FOR CASES WHEN REENTERING THE LIST?
-         */
-        selectWrapperInput.addEventListener("click", (e) => {
-            ulSelectOptions = e.target.parentNode.childNodes[1].childNodes;
-            for (let i = 0; i < ulSelectOptions.length; i++) {
-                if (ulSelectOptions[i].className == "selected" && ulSelectOptions[i].hasAttribute != "disabled" && ulSelectOptions[i].style.backgroundColor == "rgba(0, 0, 0, 0.03)") {
-                    e.target.style.cssText = classValid;
-                } else {
-                    e.target.addEventListener("focusout", () => {
-                        if (e.target.parentNode.childNodes[3].hasAttribute("required")) {
-                            if (e.target.style.borderBottom != "1px solid rgb(76, 175, 80)") {
-                                e.target.style.cssText = classInvalid;
-                            }
-                        }
-                    });
-                }
-            }
-        });
+        }
     }
 });
